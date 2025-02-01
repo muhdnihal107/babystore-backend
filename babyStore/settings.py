1a34ce9f4021694f0641986f43a929ab25dab1b6
muhdnihal <muhdnihal132@gmail.com>
2025-02-01 11:07:57 +0530

    new

--- babyStore/settings.py
+++ babyStore/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
 
 
+ALLOWED_HOSTS = ['16.171.7.195']
 # Application definition
 
 INSTALLED_APPS = [
